d4dbdd1991d28a46e667cc46fd3056a2d7733138
David Zabner <david@cazena.com>
2017-10-10 13:04:00 -0400

    rails generate devise:install then follow instructions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "paintings#index"
   resources :paintings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
