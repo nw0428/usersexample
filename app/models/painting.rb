776ee03a76e8e4020ac5d7687b061ca5f7f25bc4
David Zabner <david@cazena.com>
2017-10-18 00:28:19 -0400

    More tests

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -2,5 +2,13 @@ class Painting < ApplicationRecord
   belongs_to :owner
   has_many :comments
 
-  validates :owner, :title, :artist, presence: true
+  validates :title, presence: true
+
+  validate :year_is_in_the_past
+
+  def year_is_in_the_past
+    if (year.present? && year > DateTime.now.year)
+      errors.add(:year, "must be in the past")
+    end
+  end
 end
